b9e8f5c40a090b9383970a56b0b08bb8b95b2ddf
chocycat <chocycat@catboy.email>
2024-10-24 11:56:20 +0200

    init: project structure, lazy.nvim

---
+++ init.lua
@@ -0,0 +1,23 @@
+-- Set leader key to space
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Load lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Basic options
+require("config.options")
+
+-- Initialize lazy.nvim
+require("lazy").setup("plugins")
